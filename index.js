82eeca688abc2e97555340826619b791bfaa2b07
serref jaguraga <reef163@gmail.com>
2022-01-31 20:56:30 -0500

    commit tree 2a462e6bf9a433f970d2df648be8c8e66198b36b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const hq = 42
+function distanceFromHqInBlocks (street) {
+    return Math.abs(street - hq);
+}
+
+
+function distanceFromHqInFeet (street) {
+   return distanceFromHqInBlocks(street) * 264
+   
+}
+
+function distanceTravelledInFeet (start, destination) {
+return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const block = distanceTravelledInFeet(start, destination)
+    if (block <= 400) {
+     return 0
+    }
+    else if (block > 400 && block < 2000) {
+        return 2.56
+    } else if (block > 2500) {
+       return 'cannot travel that far'
+    } else if (block > 2000) {
+        return 25
+    }
+}
